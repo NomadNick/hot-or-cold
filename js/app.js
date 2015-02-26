bed3b923edea821c1b415c7bbcc4666e276d90aa
NomadNick <emailnick123@gmail.com>
2015-02-25 17:31:00 -0800

    added guessFeedback function, generatedNumber function

--- js/app.js
+++ js/app.js
@@ -12,6 +12,31 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
 });
 
+	var guessedNumber = $('userGuess').val();
 
+  	var generatedNumber = Math.floor((Math.random() * 100) + 1);
+
+  	var guessFeedback = function() {
+  		if (Math.abs(guessedNumber - generatedNumber) >= 50) {
+  			alert('Ice Cold!');
+  		}
+  		else if (Math.abs(guessedNumber - generatedNumber) < 50 && Math.abs(generatedNumber - guessedNumber) > 30) {
+  			alert('Cold!');
+  		}
+  		else if (Math.abs(guessedNumber - generatedNumber) <= 30 && Math.abs(generatedNumber - guessedNumber) >= 20) {
+  			alert('Warm!');
+  		}
+  		else if (Math.abs(guessedNumber - generatedNumber) < 20 && Math.abs(generatedNumber - guessedNumber) > 10) {
+  			alert('Hot!');
+  		}
+  		else {
+  			alert('Very hot!');
+  		}
+  	};
+  	$('guessButton').on(click, function () {
+  		$('#count').html(parseInt($('#count').html(), 10)+1);
+  	})
+  	
